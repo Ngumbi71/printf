d932431b8118b935341cd6de7d5898a8900748e6
Ngumbi71 <sngumbi71@gmail.com>
2023-11-14 17:59:52 -0800

    main.h

--- main.h
+++ main.h
@@ -26,17 +26,26 @@ typedef struct format
 int _putchar(char c);
 
 /*prints characters */
-int printf_char(va_list list)
-int printf_char(va_list list);
+int print_char(va_list list);
 int print_string(va_list list);
 int _strlen(char *s);
 int _strlenc(const char *s);
-int print_40(void);
-int print_s(va_list list);
-
+int print_37(void);
+int *strcpy(char *dest, char *src);
+int rev_string(char *s);
+int print_rot13(va_list list);
+int print_revs(va_list list);
+int print_bin(va_list list);
+int print_oct(va_list list);
+int print_hex(va_list list);
+int print_HEX(va_list list);
+int print_exc_string(va_list list);
+int print_HEX_extra(unsigned int num);
+int print_hex_extra(unsigned long int num);
+int print unsigned(va_list list);
+int print_pointer(va_list list);
 int print_integer(va_list list);
 int print_dec(va_list list);
-
 int _printf(const char *format, ...);
 
 #endif
